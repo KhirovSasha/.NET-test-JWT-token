f73b1564b789205562bd6166eca06fe28a329a75
Khiro Sasha <sasha.khiro@gmail.com>
2023-01-26 22:32:48 +0200

    created controller with method for hashing password

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestJWT.Models;
+using BC = BCrypt.Net;
+
+namespace TestJWT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user = new User();
+
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDto request)
+        {
+            string passwordHas = BC.BCrypt.HashPassword(request.Password);
+
+            user.Username = request.Username;
+            user.PasswordHash = passwordHas;
+
+            return Ok(user);
+        }
+    }
+}
