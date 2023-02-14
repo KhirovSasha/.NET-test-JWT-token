9365adfc74076b2cd1dd3539251e8b954f1c0f8f
Khiro Sasha <sasha.khiro@gmail.com>
2023-02-14 20:09:55 +0200

    created DbContext for connect Database and aded id property for User

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TestJWT.Models
+{
+    public class AppDbContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=TestJWT;Trusted_Connection=True;");
+        }
+    }
+}
